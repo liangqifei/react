3d32e3596bf7ceec12bfc3fcf0d6aac8aea9b4a3
lqf <1454554357@qq.com>
2018-11-16 09:41:42 +0800

    Add files via upload

---
+++ src/pages/HotMoment/HotMoment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HotMoment extends Component{
+    constructor(props){
+        super()
+    }
+    render(){
+        return (
+            <div>热门评论</div>
+        )
+    }
+}
+export default HotMoment;
